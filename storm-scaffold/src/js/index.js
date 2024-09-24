33dcd9c119377766f060dd49af8cc38dda752e72
Sarah Richards <sarah.richards@stormid.com>
2024-09-24 10:42:50 +0100

    adding scaffold src

---
+++ storm-scaffold/src/js/index.js
@@ -0,0 +1,5 @@
+// import './modules/toggle';
+
+// import(/* webpackChunkName: "toggle" */`./modules/toggle`).then(module => module.default());
+// import(/* webpackChunkName: "validate" */`@stormid/validate`).then(module => module.default.init(VALIDATE.SELECTOR));
+
---
+++ storm-scaffold/src/templates/layouts/default.js
@@ -0,0 +1,17 @@
+import { Fragment, h } from 'preact';
+import Skip from '@components/skip';
+import Header from '@components/header';
+import Main from '@components/main';
+import Footer from '@components/footer';
+
+const Default = ({ children }) => <Fragment>
+    <Header>
+        <Skip />
+    </Header>
+    <Main>
+        { children }
+    </Main>
+    <Footer />
+</Fragment>;
+
+export default Default;
