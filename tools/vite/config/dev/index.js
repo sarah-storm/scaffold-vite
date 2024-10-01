17ba0ec12dfdb02d545396246f65cc8dc319e89a
Sarah Richards <sarah.richards@stormid.com>
2024-10-01 17:35:45 +0100

    making preview work

--- tools/vite/config/dev/index.js
+++ tools/vite/config/dev/index.js
@@ -1,5 +1,7 @@
 import { defineConfig } from "vite";
 import { generateHtml } from "../../plugins";
+import path from 'path';
+import paths from "../../../../paths.config.js";
 import preact from "@preact/preset-vite";
 import base from "../base"
 import dns from 'dns';
@@ -12,6 +14,10 @@ export default defineConfig({
         preact(),
         generateHtml()
     ],
+    build: {
+        outDir: path.join(process.cwd(), paths.output),
+        emptyOutDir: true,    
+    },
     server: {
         host: 'localhost',
         port: '8081',
